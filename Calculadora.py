e8bafa431404b037f41a48f930f3c9394b585a9a
LarissaCS234 <111015194+LarissaCS234@users.noreply.github.com>
2022-10-24 17:14:57 -0300

    Create Calculadora.py

---
+++ Calculadora.py
@@ -0,0 +1,12 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+peso = float(input("Digite seu peso: "))
+
+print("-" * 30)
+print("os dados coletados foram:")
+print("Seu nome",nome)
+print("Sua idade",idade," anos")
+print("Sua altura",altura," cm")
+print("Seu peso",peso," Kg")
+print("-" * 30)
