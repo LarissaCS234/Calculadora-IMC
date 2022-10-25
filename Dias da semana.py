23a6f5cf8f4edffd5326fbe9e0332cf7ab78ced8
LarissaCS234 <111015194+LarissaCS234@users.noreply.github.com>
2022-10-25 15:37:54 -0300

    Calendario
    Calendario dos dias da semana

---
+++ Dias da semana.py
@@ -0,0 +1,15 @@
+dia = int(input("Digite um numero entre 1-7: "))
+if dia == 1:
+    print("Hoje é Domingo 😎")
+if dia == 2:
+    print("Hoje é Segunda 🙁") 
+if dia == 3: 
+    print("Hoje é Terça 😒")
+if dia == 4:
+    print("Hoje é Quarta 😐")
+if dia == 5:
+    print("Hoje é Quinta 😉")
+if dia == 6: 
+    print("Hoje é Sexta 🤩")
+if dia == 7: 
+    print("Hoje é Sabado 😀") 
