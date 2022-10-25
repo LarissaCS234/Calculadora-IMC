54a44f9f027625eab437f30ed75dcd326256b5f3
LarissaCS234 <111015194+LarissaCS234@users.noreply.github.com>
2022-10-25 15:38:23 -0300

    Calculos matematicos
    Contas de matematicas

--- Operadores matematicos.py
+++ Operadores matematicos.py
@@ -1,3 +1,6 @@
+from __future__ import unicode_literals
+
+
 n1= float(input("Digite o primeiro número: "))
 n2= float(input("Digite o segundo número: "))
 print("-" *30)
@@ -8,3 +11,4 @@ print("Divisão = ", n1 / n2)
 print("Quadrado do 1 = ", n1**2)
 print("Quadrado do 2 = ", n2**2)
 print("-" * 30)
+
