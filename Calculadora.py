df19aa1a0e55a1b10c95d44a3796a2d09bdbf72a
LarissaCS234 <111015194+LarissaCS234@users.noreply.github.com>
2022-10-25 16:41:00 -0300

    Atualização
    Acrescentado informações

--- Calculadora.py
+++ Calculadora.py
@@ -3,9 +3,9 @@ nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
 altura = float(input("Digite sua altura: "))
 peso = float(input("Digite seu peso: "))
-imc = peso/altura**2
+imc = peso/altura**2 #Formula para calcular o imc
 
-
+#saida de dados 
 print("-" * 30)
 print("os dados coletados foram:")
 print("NOME: ",nome)
@@ -13,4 +13,22 @@ print("IDADE: ",idade," anos")
 print("ALTURA: ",altura," cm")
 print("PESO: ",peso," Kgs")
 print("IMC: ", imc)
+print("-" * 30)
+
+if imc < 16:
+    print("Magreza grave!")
+elif imc >=16.1 and imc <18.5: 
+    print("Abaixo do normal")
+elif imc >18.5 and imc <24.9: 
+    print("Peso Normal")
+elif imc >25 and imc <29.9:
+    print("Excesso de peso")
+elif imc >30 and imc <34.9:
+    print("Obesidade clase I")
+elif imc >35 and imc <39.9:
+    print("Obesidade classe II")
+elif imc >=40:
+    print("Obesidade classe III")
+else: 
+    print("Dado invalido")
 print("-" * 30)
